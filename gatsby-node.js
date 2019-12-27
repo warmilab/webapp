d86bbfcdecd82ba1fc57f87e9fe35534a024c5aa
Ursu <ursula.cervantes@gmail.com>
2019-12-27 16:58:00 -0500

    fix: remove unnecessary gatsby config

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,6 @@
 
 const path = require(`path`)
 
-exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
-  if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` })
-    createNodeField({
-      node,
-      name: `slug`,
-      value: slug,
-    })
-  }
-}
-
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions
 
