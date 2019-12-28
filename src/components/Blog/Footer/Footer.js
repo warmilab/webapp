441366f86fb7a89d75c4d02ffdae217af2e6d448
Ursu <ursula.cervantes@gmail.com>
2019-12-27 20:56:36 -0500

    feat: add blog footer

---
+++ src/components/Blog/Footer/Footer.js
@@ -0,0 +1,14 @@
+import { Link } from "gatsby"
+import React from "react"
+
+const Footer = ({ siteTitle }) => (
+  <div className="text-center mt-5">
+    <p> ・  ・  ・ </p>
+    <p>
+      ¿Quieres escribir en nuestro blog? Entérate <Link to="/blog/info">aquí</Link> cómo.
+    </p>
+  </div>
+)
+
+
+export default Footer
--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -3,6 +3,7 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Footer from "../components/Blog/Footer"
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -52,6 +53,7 @@ export default function Template({
               dangerouslySetInnerHTML={{ __html: html }}
             />
           </div>
+          <Footer />
         </div>
     </Layout>
   )
